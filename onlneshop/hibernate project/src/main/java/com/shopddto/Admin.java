e18217685933ee72ce4eeff56a28a352679e4528
sri1-0 <139852671+sri1-0@users.noreply.github.com>
2023-08-10 11:56:07 +0530

    Add files via upload

---
+++ onlneshop/hibernate project/src/main/java/com/shopddto/Admin.java
@@ -0,0 +1,79 @@
+package com.shopddto;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int adminId;
+	private String adminName;
+	private String adminEmail;
+	private String adminPassword;
+	private String adminContact;
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Item> adminItem;
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	public String getAdminEmail() {
+		return adminEmail;
+	}
+
+	public void setAdminEmail(String adminEmail) {
+		this.adminEmail = adminEmail;
+	}
+
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+
+	public String getAdminContact() {
+		return adminContact;
+	}
+
+	public void setAdminContact(String adminContact) {
+		this.adminContact = adminContact;
+	}
+
+	public List<Item> getAdminItem() {
+		return adminItem;
+	}
+
+	public void setAdminItem(List<Item> adminItem) {
+		this.adminItem = adminItem;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", adminName=" + adminName + ", adminEmail=" + adminEmail
+				+ ", adminPassword=" + adminPassword + ", adminContact=" + adminContact + ", adminItem=" + adminItem
+				+ "]";
+	}
+
+}
